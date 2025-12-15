0c54e1ffc3db2236b24efc0e2786398ad5ebdf0e
Giorgia3 <26458245+Giorgia3@users.noreply.github.com>
2025-12-15 19:01:51 +0100

    updated bwa paths

--- src/utils/bwa.sh
+++ src/utils/bwa.sh
@@ -1,5 +1,5 @@
 # !/usr/bin/env sh
 pwd
-/home/giorgia/miniconda3/bin/bwa index datasets/reference.fasta
-/home/giorgia/miniconda3/bin/bwa mem datasets/reference.fasta experiment/preprocessed_data/reformatted_seqs.fasta > experiment/preprocessed_data/aligned.sam
+/root/miniconda3/bin/bwa index datasets/reference.fasta
+/root/miniconda3/bin/bwa mem datasets/reference.fasta experiment/preprocessed_data/reformatted_seqs.fasta > experiment/preprocessed_data/aligned.sam
 exit
